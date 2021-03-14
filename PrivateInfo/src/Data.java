43162952695d9c2d4a346d536d03360555ad3c9b
TOSHIBA <TOSHIBA@DESKTOP-SEP3065>
2021-03-14 09:20:00 +0300

    The third object

---
+++ PrivateInfo/src/Data.java
@@ -0,0 +1,36 @@
+
+public class Data {
+
+	
+	private String username;
+	private int pass;
+    String wel;
+
+
+	Data(){                  //constructor 
+		
+	}
+	
+	Data(String nnewuser , int newpass){                   
+		
+		username = nnewuser;
+		pass = newpass;
+	}
+	//public void setid(int nnewpass){  //Setter
+
+		 // this.pass = nnewpass;
+
+//	}	
+	public String getusername(){             //Getter
+		 
+		 return username;
+	}
+	public int getpass(){             //Getter
+		 
+		 return pass;
+	}
+	 public void wel() {
+		
+		System.out.println("Hi Nawaf");
+	}
+}
---
+++ PrivateInfo/src/Testdata.java
@@ -0,0 +1,14 @@
+
+public class Testdata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Data one = new Data("Nawaf", 1234);
+		
+		System.out.println(one.getusername());
+		System.out.println(one.getpass());
+		one.wel();
+	}
+
+}
